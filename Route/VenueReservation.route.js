e38946b90c4c84311a6e4f95a0defa501a58b6e9
deithercasagnap <d.casagnap1@gmail.com>
2024-03-14 22:54:04 +0800

    added get, patch and delete in venue reservation

--- Route/VenueReservation.route.js
+++ Route/VenueReservation.route.js
@@ -15,4 +15,58 @@ router.post('/', async (req, res, next) => {
     }
 });
 
+
+router.get('/', async (req, res, next) => {
+    try {
+       
+        const result = await VenueReservation.find({}, {__v:0});
+        res.send(result);
+    } catch (error) {
+        res.send(error.message);
+        
+    }
+});
+
+
+router.get('/:id', async (req, res, next) => {
+    const id = req.params.id;
+    try {
+       
+        const result = await VenueReservation.findById(id);
+        res.send(result);
+    } catch (error) {
+        res.send(error.message);
+        
+    }
+});
+
+
+router.patch('/:id', async (req, res, next) => {
+    
+
+    try {
+        const id = req.params.id;
+        const updates = req.body;
+        const result = await VenueReservation.findByIdAndUpdate(id, updates);
+        res.send(result);
+    } catch (error) {
+        res.send(error.message);
+        
+    }
+});
+
+
+router.delete('/:id', async (req, res, next) => {
+    const id = req.params.id;
+    try {
+       
+        const result = await VenueReservation.findByIdAndDelete(id);
+        res.send(result);
+    } catch (error) {
+        res.send(error.message);
+        
+    }
+});
+
+
 module.exports = router;
